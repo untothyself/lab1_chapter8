38299014fc28dd4d801261091ea5934e96d1662c
Abass Hassan <Ahassan124@student.cscc.edu>
2026-06-19 16:40:27 -0400

    rectangle equation

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+""" 
+Program: rectangle function
+Author: Abass Hassan
+Purpose: calculate rectangle area and perimeter
+Date: June 2026"""
+
+def calc_area(length, width):
+    """Calculate the area of a rectangle."""
+    return length * width
+
+def calc_perimeter(length, width):
+    """Calculate the perimeter of a rectangle."""
+    return 2 * (length + width)
