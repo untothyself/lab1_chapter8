44601a05fbe739d99987ecaf5507be1acae035ad
Abass Hassan <Ahassan124@student.cscc.edu>
2026-06-19 16:35:14 -0400

    Circle equation

---
+++ circle.py
@@ -0,0 +1,13 @@
+"""
+Program: Circle Function
+Author: Abass Hassan
+Purpose: calculate circle area and circumference
+Date: June 2026"""
+
+import math
+def calc_area(radius):
+    """Calculate the area of a circle."""
+    return math.pi * radius ** 2
+def calc_circumference(radius):
+    """Calculate the circumference of a circle"""
+    return 2 * math.pi * radius
